69d264a752448abea2ef52b445106433ccf22881
alejandra palza morales <alejandra.palza@ucb.edu.bo>
2024-09-05 00:50:23 -0400

    Validación Posicion Inicial dentro de la matriz

--- src/Controlador.test.js
+++ src/Controlador.test.js
@@ -22,5 +22,21 @@ describe("Revision del formato matriz + posicion inicial ", () => {
         it ("Posicion Inicial Incorrecta Controlador.ts", () => {
             expect(revision("1.2n")).toEqual(false);
         });
-        });
+    });
+});
+
+describe("Revision posicion inicial dentro matriz", () => {
+    it ("Posicion Inicial Dentro de la Matriz", () => {
+        expect(revision("5,5/1,2N")).toEqual("1,2N");
+    });
+    it ("Posicion Inicial Dentro de la Matriz en el limite", () => {
+        expect(revision("5,5/5,5N")).toEqual("5,5N");
+    });
+
+    it ("Posicion Inicial Incorrecta Fuera de la Matriz en X", () => {
+        expect(revision("5,5/6,2N")).toEqual(false);
+    });
+    it ("Posicion Inicial Incorrecta Fuera de la Matriz en Y", () => {
+        expect(revision("5,5/3,6N")).toEqual(false);
+    });
 });
--- src/RevisionFormato.js
+++ src/RevisionFormato.js
@@ -4,13 +4,20 @@ function revision(comandos){
     // Intenta hacer coincidir la cadena de entrada con la expresión
     const coincidencia = comandos.match(formatoRegex);
     if (coincidencia) {
+        const posX = coincidencia[1];
+        const posY = coincidencia[2];
         const posXI = coincidencia[3];
         const posYI = coincidencia[4];
+        if(posXI<=posX &&  posYI<=posY){
         const direccion = coincidencia[5];
         const cadenaDevolucion = `${posXI},${posYI}${direccion}`;
         return cadenaDevolucion;
-  }
+        }
+}
+
 // La cadena de entrada no tiene el formato correcto
   return false;
 }
+
+
 export default revision;
