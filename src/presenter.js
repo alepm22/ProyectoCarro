50d776d2b6c960f4ca15642dfe5d2206fc045f45
sergioBustamante <sergio.bustamante@niceincontact.com>
2024-09-05 06:16:27 -0400

    Refactorización, separación de lógica en funciones y manejo de eventos en Presenter.js

--- src/presenter.js
+++ src/presenter.js
@@ -1,5 +1,5 @@
-import revision from "./RevisionFormato"
-import controlador from "./Controlador"
+import revision from "./RevisionFormato";
+import controlador from "./Controlador";
 
 const cadena = document.querySelector("#Cadena");
 const form = document.querySelector("#Controlador-form");
@@ -8,24 +8,30 @@ const comandos = document.querySelector("#Comandos");
 const posicion_final = document.querySelector("#Posicion_final");
 const mensajes = document.querySelector("#mensajes");
 
-form.addEventListener("input", (event) => {
-  event.preventDefault();
-  const cadenaValor = cadena.value || ""; // Si cadena.value es undefined usa una vacia
-  const resultadoRevision = revision(cadenaValor);
+const mostrarMensaje = (mensaje) => {
+  mensajes.innerHTML = `<p>${mensaje}</p>`;
+};
 
-  if (resultadoRevision) {// Si la cadena es válida, muestra la posición inicial y los comandos
+const actualizarPosicionInicialYComandos = () => {
+  const cadenaValor = cadena.value || "";
+  const resultadoRevision = revision(cadenaValor);
+  
+  if (resultadoRevision) {
     posicion_inicial.innerHTML = `<p>Posición inicial: ${resultadoRevision.posicion_inicial}</p>`;
     comandos.innerHTML = `<p>Comandos: ${resultadoRevision.comandos_movimientos}</p>`;
-    form.addEventListener("submit", (event) => { //presionar boton ejecutar
-      event.preventDefault();
-      posicion_final.innerHTML = "<p> Posicion Final:" + controlador(cadena.value) + "</p>";
-    });
-    mensajes.innerHTML = "<p> Mensajes:  </p>";
-  }
-
-  else {// Si la cadena no es válida
+    mostrarMensaje("");
+  } else {
     posicion_inicial.innerHTML = "<p> Posición Inicial:</p>";
-    comandos.innerHTML ="<p> Comandos:</p>";
-    mensajes.innerHTML = "<p> Mensajes: Error en el formato  </p>";
+    comandos.innerHTML = "<p> Comandos:</p>";
+    mostrarMensaje("Error en el formato");
   }
+};
+
+form.addEventListener("input", () => {
+  actualizarPosicionInicialYComandos();
+});
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  posicion_final.innerHTML = `<p>Posición Final: ${controlador(cadena.value)}</p>`;
 });
